e4045a7815389e41d79a95d97c6cdb4292dc78c3
dkssall <79607103+dkssall@users.noreply.github.com>
2026-08-16 20:34:08 +0300

    Update booking.dart

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -1,1 +1,17 @@
+class Booking {
+  final String id;
+  final String service;
+  final String name;
+  final String phone;
+  final String date;
+  final String time;
 
+  Booking({
+    required this.id,
+    required this.service,
+    required this.name,
+    required this.phone,
+    required this.date,
+    required this.time,
+  });
+}
