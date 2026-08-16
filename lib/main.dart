865c986fc70727edc3dd4d74a340a6e8c39f77dc
dkssall <79607103+dkssall@users.noreply.github.com>
2026-08-16 21:15:56 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -863,7 +863,56 @@ Future<void> _showBookingDialog() async {
   timeController.dispose();
 }
   
+Widget _bookingButton() {
+  return GestureDetector(
+    onTap: () {
+      setState(() => selectedIndex = 2);
+      _showBookingDialog();
+    },
+    child: Transform.translate(
+      offset: const Offset(0, -18),
+      child: Container(
+        width: 72,
+        height: 72,
+        decoration: BoxDecoration(
+          color: AjelApp.blue,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.white,
+            width: 5,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: AjelApp.blue.withOpacity(.30),
+              blurRadius: 15,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.luggage_outlined,
+              color: Colors.white,
+              size: 30,
+            ),
+            Text(
+              'حجوزات',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 11,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
 
+}
 class DottedLinePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
