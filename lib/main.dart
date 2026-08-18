3acd2118866659ebb85526f7ccc122eaa1bbaad1
dkssall <79607103+dkssall@users.noreply.github.com>
2026-08-18 22:34:14 +0300

    Add Firebase Database import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'models/booking.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_database/firebase_database.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
